6d9a286f38b74cd9d4d03ca4bea35ece4b3c2e20
Mark Smith <mark.sm.smith@oracle.com>
2018-07-19 07:11:43 -0600

    add apache recipe to recipes/default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,16 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+# This is my first attempt at using Nano
+# This is my second attempt at using Nano
+
+package 'httpd'
+
+file '/var/www/html/index.html' do
+  content <h1>Hello World!</h1>
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
